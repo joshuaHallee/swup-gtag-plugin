b35c76bb66da88ad0315e138bde61aa0bc651194
Joshua Hallee <jthallee@email.neit.edu>
2020-02-29 19:55:28 -0500

    improve error handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import Plugin from '@swup/plugin';
 
-export default class GtagPlugin extends Plugin {
-	name = 'GtagPlugin';
+export default class SwupGtagPlugin extends Plugin {
+	name = 'SwupGtagPlugin';
 
 	constructor(options) {
 		super();
-		const defaultOptions = {
-			gaMeasurementId: ''
-		};
+
+		const defaultOptions = {};
 
 		this.options = {
 			...defaultOptions,
@@ -16,18 +15,24 @@ export default class GtagPlugin extends Plugin {
 	}
 
 	mount() {
-		if (typeof window.gtag === 'function' && this.options.gaMeasurementId != '') {
-			let title = document.title;
-			let url = window.location.pathname + window.location.search;
-
-			window.gtag("config", `${this.options.gaMeasurementId}`, {
-				page_title: title,
-				page_path: url
-			})
-
-			this.swup.log(`Gtag pageview (url '${url}').`);
-		} else {
-			console.warn('Gtag is not loaded.');
+		if(this.options.gaMeasurementId == undefined) {
+			console.warn('Gtag is not configured with a GA_MEASUREMENT_ID');
 		}
+
+		this.swup.on('contentReplaced', event => {
+			if (typeof window.gtag === 'function' && this.options.gaMeasurementId != undefined) {
+				let title = document.title;
+				let url = window.location.pathname + window.location.search;
+
+				window.gtag("config", `${this.options.gaMeasurementId}`, {
+					page_title: title,
+					page_path: url
+				})
+
+				this.swup.log(`Gtag pageview (url '${url}').`);
+			} else {
+				console.warn("Gtag is not loaded.")
+			}
+		})
 	}
 }
